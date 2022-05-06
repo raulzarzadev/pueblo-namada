ba7c69823c8af8f5232fd211c05076e44dde1187
zasara <raulzarza@gmail.com>
2022-05-06 15:24:55 -0600

    addign hidden bottom nav bar

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,6 +3,7 @@ import MobileNav from '../MobileNav'
 import { useUser } from '@comps/context/userContext'
 import { useRouter } from 'next/router'
 import TopNavBar from './TopNavBar'
+import { useEffect, useState } from 'react'
 export default function Layout({ children }) {
   const { user } = useUser()
 
@@ -29,7 +30,35 @@ export default function Layout({ children }) {
   } else if (avoidWhenLogged && user) {
     push('/')
   }
+  const [lastScroll, setLastScroll] = useState(0);
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scroll = window.scrollY;
+      if (scroll > lastScroll) {
+        // scroll is going down
+        setShowMobileNav(false);
+      } else {
+        // scroll is going up
+        setShowMobileNav(true);
+      }
+      setLastScroll(scroll)
+    };
 
+    // just trigger this so that the initial state 
+    // is updated as soon as the component is mounted
+    // related: https://stackoverflow.com/a/63408216
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(showMobileNav)
 
   return (
     <div className='relative bg-base-300' >
@@ -40,7 +69,7 @@ export default function Layout({ children }) {
       <main>
         {children}
       </main>
-      <footer className='relative'>
+      <footer className={`relative   ${showMobileNav ? `-translate-x-0` : ``} `}>
         <MobileNav />
       </footer>
     </div>)
