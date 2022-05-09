812ffa68920afa1f21c3b920b78d4c5df06ed9ce
zasara <raulzarza@gmail.com>
2022-05-09 15:09:15 -0600

    agregar nuevo espacio, limitar acceso si no es ownre

--- pages/places/[id]/dashboard/index.js
+++ pages/places/[id]/dashboard/index.js
@@ -1,19 +1,32 @@
 import { useRouter } from "next/router"
+import { useUser } from '@comps/context/userContext'
 import { useEffect, useState } from "react"
 import FormPlaceConfig from "../../../../components/FormPlaceConfig"
 import PlaceDetails from "../../../../components/Places/PlaceDetails"
 import { listenPlace } from "../../../../firebase/places"
 
 export default function dashboard() {
-
   const [place, setPlace] = useState(undefined)
-  const { query: { id } } = useRouter()
+  const { query: { id }, back } = useRouter()
   useEffect(() => {
     listenPlace(id, setPlace)
   }, [])
 
+  const { user } = useUser()
 
-  console.log(place)
+  const isOwner = place?.owner === user.id
+
+  const handleBack = () => {
+    back()
+  }
+
+  if (!isOwner) return <div>
+    <p className="text-center font-bold my-10">No tienes permisos para ver este lugar</p>
+    <button className="btn btn-outline" onClick={() => handleBack()} >Regresar</button>
+  </div>
+
+
+
   return (
     <div>
       <h1 className="text-center font-bold border">Dashboard</h1>
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { useEffect, useState } from "react"
 import PlaceCard from "../../components/Places/PlaceCard"
 import { listenUserPlaces } from "../../firebase/places"
@@ -12,11 +13,19 @@ export default function Profile() {
   console.log(places)
 
 
+
   return (
     <div>
       <h1 className="text-center">Perfil de usuario</h1>
       <div>
         <h2 className="text-center">Mis Lugares</h2>
+        <div className="flex w-full justify-center p-2">
+          <Link href='/new-place'>
+            <a className="btn btn-info btn-sm">
+              Nuevo lugar
+            </a>
+          </Link>
+        </div>
         {places?.map(place =>
           <PlaceCard place={place} linkToDashboard />)}
       </div>
