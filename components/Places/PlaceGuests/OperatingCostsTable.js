2d3339b843fdb597597afce28a9c1d63d8bbbb69
zasara <raulzarza@gmail.com>
2022-06-09 12:53:47 -0600

    add operatign costes

---
+++ components/Places/PlaceGuests/OperatingCostsTable.js
@@ -0,0 +1,11 @@
+const OperatingCostsTable = ({ place }) => {
+  console.log(place)
+  return (
+    <div className="my-4">
+      <h2 className="font-bold text-center">Operating Costs Table</h2>
+
+    </div>
+  )
+}
+
+export default OperatingCostsTable
